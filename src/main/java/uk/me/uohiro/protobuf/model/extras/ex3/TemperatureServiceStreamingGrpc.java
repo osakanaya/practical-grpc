f930e1dff24054b141f5d36fbd9f267eec8f163d
Hideo Sashida <osakanaya@gmail.com>
2018-12-10 16:37:34 +0900

    Extras ex3: bidirectional streaming dependencies

---
+++ src/main/java/uk/me/uohiro/protobuf/model/extras/ex3/TemperatureServiceStreamingGrpc.java
@@ -0,0 +1,264 @@
+package uk.me.uohiro.protobuf.model.extras.ex3;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.16.1)",
+    comments = "Source: extras/ex3/weather.proto")
+public final class TemperatureServiceStreamingGrpc {
+
+  private TemperatureServiceStreamingGrpc() {}
+
+  public static final String SERVICE_NAME = "extras.ex3.TemperatureServiceStreaming";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+      uk.me.uohiro.protobuf.model.extras.ex3.Temperature> getObserveMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "Observe",
+      requestType = uk.me.uohiro.protobuf.model.extras.ex3.Coordinates.class,
+      responseType = uk.me.uohiro.protobuf.model.extras.ex3.Temperature.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+  public static io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+      uk.me.uohiro.protobuf.model.extras.ex3.Temperature> getObserveMethod() {
+    io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates, uk.me.uohiro.protobuf.model.extras.ex3.Temperature> getObserveMethod;
+    if ((getObserveMethod = TemperatureServiceStreamingGrpc.getObserveMethod) == null) {
+      synchronized (TemperatureServiceStreamingGrpc.class) {
+        if ((getObserveMethod = TemperatureServiceStreamingGrpc.getObserveMethod) == null) {
+          TemperatureServiceStreamingGrpc.getObserveMethod = getObserveMethod = 
+              io.grpc.MethodDescriptor.<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates, uk.me.uohiro.protobuf.model.extras.ex3.Temperature>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+              .setFullMethodName(generateFullMethodName(
+                  "extras.ex3.TemperatureServiceStreaming", "Observe"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  uk.me.uohiro.protobuf.model.extras.ex3.Coordinates.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  uk.me.uohiro.protobuf.model.extras.ex3.Temperature.getDefaultInstance()))
+                  .setSchemaDescriptor(new TemperatureServiceStreamingMethodDescriptorSupplier("Observe"))
+                  .build();
+          }
+        }
+     }
+     return getObserveMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static TemperatureServiceStreamingStub newStub(io.grpc.Channel channel) {
+    return new TemperatureServiceStreamingStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static TemperatureServiceStreamingBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new TemperatureServiceStreamingBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static TemperatureServiceStreamingFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new TemperatureServiceStreamingFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class TemperatureServiceStreamingImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates> observe(
+        io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Temperature> responseObserver) {
+      return asyncUnimplementedStreamingCall(getObserveMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getObserveMethod(),
+            asyncBidiStreamingCall(
+              new MethodHandlers<
+                uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+                uk.me.uohiro.protobuf.model.extras.ex3.Temperature>(
+                  this, METHODID_OBSERVE)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class TemperatureServiceStreamingStub extends io.grpc.stub.AbstractStub<TemperatureServiceStreamingStub> {
+    private TemperatureServiceStreamingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private TemperatureServiceStreamingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected TemperatureServiceStreamingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new TemperatureServiceStreamingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates> observe(
+        io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Temperature> responseObserver) {
+      return asyncBidiStreamingCall(
+          getChannel().newCall(getObserveMethod(), getCallOptions()), responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class TemperatureServiceStreamingBlockingStub extends io.grpc.stub.AbstractStub<TemperatureServiceStreamingBlockingStub> {
+    private TemperatureServiceStreamingBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private TemperatureServiceStreamingBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected TemperatureServiceStreamingBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new TemperatureServiceStreamingBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class TemperatureServiceStreamingFutureStub extends io.grpc.stub.AbstractStub<TemperatureServiceStreamingFutureStub> {
+    private TemperatureServiceStreamingFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private TemperatureServiceStreamingFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected TemperatureServiceStreamingFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new TemperatureServiceStreamingFutureStub(channel, callOptions);
+    }
+  }
+
+  private static final int METHODID_OBSERVE = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final TemperatureServiceStreamingImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(TemperatureServiceStreamingImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_OBSERVE:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.observe(
+              (io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Temperature>) responseObserver);
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class TemperatureServiceStreamingBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    TemperatureServiceStreamingBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return uk.me.uohiro.protobuf.model.extras.ex3.WeatherAPIProto.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("TemperatureServiceStreaming");
+    }
+  }
+
+  private static final class TemperatureServiceStreamingFileDescriptorSupplier
+      extends TemperatureServiceStreamingBaseDescriptorSupplier {
+    TemperatureServiceStreamingFileDescriptorSupplier() {}
+  }
+
+  private static final class TemperatureServiceStreamingMethodDescriptorSupplier
+      extends TemperatureServiceStreamingBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    TemperatureServiceStreamingMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (TemperatureServiceStreamingGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new TemperatureServiceStreamingFileDescriptorSupplier())
+              .addMethod(getObserveMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
---
+++ src/main/java/uk/me/uohiro/protobuf/model/extras/ex3/WindServiceStreamingGrpc.java
@@ -0,0 +1,264 @@
+package uk.me.uohiro.protobuf.model.extras.ex3;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.16.1)",
+    comments = "Source: extras/ex3/weather.proto")
+public final class WindServiceStreamingGrpc {
+
+  private WindServiceStreamingGrpc() {}
+
+  public static final String SERVICE_NAME = "extras.ex3.WindServiceStreaming";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+      uk.me.uohiro.protobuf.model.extras.ex3.Wind> getObserveMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "Observe",
+      requestType = uk.me.uohiro.protobuf.model.extras.ex3.Coordinates.class,
+      responseType = uk.me.uohiro.protobuf.model.extras.ex3.Wind.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+  public static io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+      uk.me.uohiro.protobuf.model.extras.ex3.Wind> getObserveMethod() {
+    io.grpc.MethodDescriptor<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates, uk.me.uohiro.protobuf.model.extras.ex3.Wind> getObserveMethod;
+    if ((getObserveMethod = WindServiceStreamingGrpc.getObserveMethod) == null) {
+      synchronized (WindServiceStreamingGrpc.class) {
+        if ((getObserveMethod = WindServiceStreamingGrpc.getObserveMethod) == null) {
+          WindServiceStreamingGrpc.getObserveMethod = getObserveMethod = 
+              io.grpc.MethodDescriptor.<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates, uk.me.uohiro.protobuf.model.extras.ex3.Wind>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+              .setFullMethodName(generateFullMethodName(
+                  "extras.ex3.WindServiceStreaming", "Observe"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  uk.me.uohiro.protobuf.model.extras.ex3.Coordinates.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  uk.me.uohiro.protobuf.model.extras.ex3.Wind.getDefaultInstance()))
+                  .setSchemaDescriptor(new WindServiceStreamingMethodDescriptorSupplier("Observe"))
+                  .build();
+          }
+        }
+     }
+     return getObserveMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static WindServiceStreamingStub newStub(io.grpc.Channel channel) {
+    return new WindServiceStreamingStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static WindServiceStreamingBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new WindServiceStreamingBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static WindServiceStreamingFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new WindServiceStreamingFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class WindServiceStreamingImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates> observe(
+        io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Wind> responseObserver) {
+      return asyncUnimplementedStreamingCall(getObserveMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getObserveMethod(),
+            asyncBidiStreamingCall(
+              new MethodHandlers<
+                uk.me.uohiro.protobuf.model.extras.ex3.Coordinates,
+                uk.me.uohiro.protobuf.model.extras.ex3.Wind>(
+                  this, METHODID_OBSERVE)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class WindServiceStreamingStub extends io.grpc.stub.AbstractStub<WindServiceStreamingStub> {
+    private WindServiceStreamingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private WindServiceStreamingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected WindServiceStreamingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new WindServiceStreamingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Coordinates> observe(
+        io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Wind> responseObserver) {
+      return asyncBidiStreamingCall(
+          getChannel().newCall(getObserveMethod(), getCallOptions()), responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class WindServiceStreamingBlockingStub extends io.grpc.stub.AbstractStub<WindServiceStreamingBlockingStub> {
+    private WindServiceStreamingBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private WindServiceStreamingBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected WindServiceStreamingBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new WindServiceStreamingBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class WindServiceStreamingFutureStub extends io.grpc.stub.AbstractStub<WindServiceStreamingFutureStub> {
+    private WindServiceStreamingFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private WindServiceStreamingFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected WindServiceStreamingFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new WindServiceStreamingFutureStub(channel, callOptions);
+    }
+  }
+
+  private static final int METHODID_OBSERVE = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final WindServiceStreamingImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(WindServiceStreamingImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_OBSERVE:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.observe(
+              (io.grpc.stub.StreamObserver<uk.me.uohiro.protobuf.model.extras.ex3.Wind>) responseObserver);
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class WindServiceStreamingBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    WindServiceStreamingBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return uk.me.uohiro.protobuf.model.extras.ex3.WeatherAPIProto.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("WindServiceStreaming");
+    }
+  }
+
+  private static final class WindServiceStreamingFileDescriptorSupplier
+      extends WindServiceStreamingBaseDescriptorSupplier {
+    WindServiceStreamingFileDescriptorSupplier() {}
+  }
+
+  private static final class WindServiceStreamingMethodDescriptorSupplier
+      extends WindServiceStreamingBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    WindServiceStreamingMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (WindServiceStreamingGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new WindServiceStreamingFileDescriptorSupplier())
+              .addMethod(getObserveMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
